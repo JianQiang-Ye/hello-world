40e70e5e568cf971e8cfa0e32821a64d04d9195f
JianQiang-Ye <759346463@qq.com>
2023-09-18 19:27:56 +0800

    Add files via upload

---
+++ src/main/java/com/jirengu/PrimitiveDataTypesPractice.java
@@ -0,0 +1,21 @@
+public class Test1 {
+    public static void main(String[] args) {
+        byte age = 10;
+        short height = 30000;
+        int num = 1000000000;
+        long distance = 10000000000L;
+        float rate = 6/5f;
+        double fixedRate = 456/123d;
+        char ch = 'A';
+        boolean isUpdated = true;
+
+        System.out.println(age);
+        System.out.println(height);
+        System.out.println(num);
+        System.out.println(distance);
+        System.out.println(rate);
+        System.out.println(fixedRate);
+        System.out.println(ch);
+        System.out.println(isUpdated);
+    }
+}
